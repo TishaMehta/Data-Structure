a24f1d17720bd1e50e7aeb71beb22aca36f9ed57
TishaMehta <105579992+TishaMehta@users.noreply.github.com>
2022-07-08 03:19:09 -1100

    Add files via upload

--- Lab3/Lab3_1.java
+++ Lab3/Lab3_1.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class Lab3_1{
+	public static void swap(int a, int b){
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("1st number:"+a);
+		System.out.println("2nd number:"+b);
+	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("entre n1:");
-		int n1=sc.nextInt();
-		System.out.println("entre n2:");
-		int n2=sc.nextInt();
-		int temp=n1;
-		n1=n2;
-		n2=temp;
-		System.out.println("n1:"+n1);
-		System.out.println("n2:"+n2);
+		System.out.println("entre a:");
+		int a=sc.nextInt();
+		System.out.println("entre b:");
+		int b=sc.nextInt();
+		swap(a,b);
 	}
 }
